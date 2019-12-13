02ed6115527c1aa3eca1aff871b3cb0bc757b948
Jun Ni <jun.ni@tanan.net.cn>
2019-12-14 04:15:11 +0800

    docker
    - README.mk update.

--- README.mk
+++ README.mk
@@ -1,3 +1,9 @@
-Add git configuration:
-	git config --local commit.template .git/COMMIT_TPL
+
+Add below statements to .profile to levarage scripts under _bin/.
+
+# Register docker helper scripts.
+# Suppose repo is cloned under /content.
+if [ -d "/content/docker/_bin" ] ; then
+    PATH="/content/docker/_bin:$PATH"
+fi
 
--- _bin/.profile
+++
@@ -1,5 +0,0 @@
-# Register docker helper scripts.
-if [ -d "/content/docker/bin" ] ; then
-    PATH="/content/docker/bin:$PATH"
-fi
-
